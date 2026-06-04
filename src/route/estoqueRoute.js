cdae5337959e283c98344c15cb8d425c2e0978b3
arnaldocnt <a.correa.nt@gmail.com>
2026-06-04 18:53:29 -0300

    Teste de rotas

--- src/route/estoqueRoute.js
+++ src/route/estoqueRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const estoqueController = require('../controller/estoqueController');
 
+router.get("/teste", (req, res) => {
+    res.json({ mensagem: "A rota /api/teste está funcionando perfeitamente no Render!" });
+});
+
 router.get("/estoque", estoqueController.listarEstoque);
 router.get("/estoque/:id", estoqueController.buscarProduto);
 router.put("/estoque/:id", estoqueController.atualizarProduto);
